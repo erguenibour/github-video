ec6ca74326b97b38c0c533c192cf2172ceca6fb8
erguenibour <erguenibour01@gmail.com>
2023-10-07 10:34:50 +0200

    Notion de tkinter

---
+++ fenetre.py
@@ -0,0 +1,18 @@
+import tkinter
+#from tkinter import *
+
+mainapp = tkinter.Tk()
+mainapp.title('Ma première fenetre')
+#mainapp.minsize(640,480)
+#mainapp.maxsize(1280,720)
+#mainapp.resizable(width=False, height=False)
+#mainapp.positionfrom("user")
+#mainapp.geometry("800x600+50+100")
+
+"""
+pour centrer la fenetre en python il faut passer par des calcules 
+position_X= (largeur//2)-(lareur_fenetre//2)
+position_y= (hauteureur//2)-(hauteur_fenetre//2)
+"""
+mainapp.mainloop()
+
